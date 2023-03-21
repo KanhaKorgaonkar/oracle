698ad84fe3864d34315c0e8630336156dffbdc0d
Kanha Korgaonkar <83264637+KanhaKorgaonkar@users.noreply.github.com>
2023-03-21 12:21:15 +0000

    Caller contract complete! 🏆

--- contracts/caller/Caller.sol
+++ contracts/caller/Caller.sol
@@ -20,9 +20,40 @@ contract Caller is Ownable {
         require(msg.sender == address(randOracle), "Unauthorized.");
         _;
     }
+    // Set the address of the oracle contract.
     function setRandOracleAddress(address newAddress) external onlyOwner {
+        // create an instance of iRANDOracle interface with an address that's provided
         randOracle = IRandOracle(newAddress);
+   // event emited to let users know that a change has been made to the contract (for transparency)
         emit OracleAddressChanged(newAddress);
+        // 
+    }
+
+    function getRandomNumber() external {
+        // condition which requires that address of randOracle is not equal to null address (address of uninitialized contract references).
+        require(randOracle != IRandOracle(address(0)), "Oracle not initialized." );
+        // Call requestRandomNumber() which from IRandOracle which returns a request ID. 
+        uint256 id = randOracle.requestRandomNumber();
+        // mark id as valid in requests mapping.
+        requests[id] = true;
+        // emit an event to show that a random number has been requested.
+        emit RandomNumberRequested(id);
+        
+    }
+
+    function fulfillRandomNumberRequest(uint256 randomNumber, uint256 id) external onlyRandOracle {
+        // requires requestID. 
+        require(requests[id], "Request is invalid or already fulfilled.");
+        // stores the randomnumber in the result mapping 
+        results[id] = randomNumber;
+        // delete the request ID after the request has been fulfilled.
+        delete requests[id];
+        // emit an event to announce that the request has been fulfilled.
+        emit RandomNumberReceived(randomNumber, id);
+        
     }
     
+    event OracleAddressChanged(address oracleAddress);
+    event RandomNumberRequested(uint256 id);
+    event RandomNumberReceived(uint256 number, uint256 id);
 }
